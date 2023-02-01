5606f4607eac61fbc7bad25c7749975b7006b289
rrjr0306 <rrjr0306@gmail.com>
2023-02-01 01:52:18 -0500

    more cart functionality...

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,29 +1,69 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-
-
-// display shoes that are in order_products that have an userId of 
-//join ? how to reference the userId from the orderId that order_products is referencing
-
-
-// take off price from order_products (already on shoes)
-// what is join???
 
 const Cart = ({cartProducts, setCartProducts}) => {
 
+let localCart = localStorage.getItem("cart");
+console.log('what the hell?', JSON.parse(localCart))
 
-    // useEffect(() => {
-    //     window.localStorage.setItem('cart', cartProducts);
-    // }, [cartProducts])
+const addShoe = (shoe) => {
+    let cartCopy = [...cartProducts];
 
-// const cartItem = [{
-//     id: cartProducts.id,
-//     name: cartProducts.shoename,
-//     description: cartProducts.description,
-//     price: cartProducts.price,
-//     image: cartProducts.image
-// }]console.log('cartItem', cartItem)
+    let {ID} = shoe;
 
+    let existingShoe = cartCopy.find(cartShoe => cartShoe.ID == ID);
+
+    if (existingShoe) {
+        existingShoe.quantity += shoe.quantity
+    } else {
+        cartCopy.push(shoe)
+    }
+
+    setCartProducts(cartCopy)
+
+    let stringCart = JSON.stringify(cartCopy);
+    localStorage.setItem("cart", stringCart)
+}
+
+const editShoe = (shoeID, amount) => {
+    let cartCopy = [...cartProducts]
+
+    let existingItem = cartCopy.find(cartShoe => cartShoe.ID == shoeID)
+
+    if (!existingItem) return
+
+    existingItem.quantity += amount;
+
+    if (existingItem.quantity <= 0) {
+        cartCopy = cartCopy.filter(cartShoe => cartShoe.ID != shoeID)
+    }
+
+    setCartProducts(cartCopy);
+
+    let cartString = JSON.stringify(cartCopy)
+    localStorage.setItem("cart", cartString)
+}
+
+const removeItem = (shoeID) => {
+    let cartCopy = [...cartProducts]
+
+    cartCopy = cartCopy.filter(cartShoe => cartShoe.ID != shoeID);
+    console.log('cartshoe', cartShoe)
+    setCartProducts(cartCopy);
+
+    let cartString = JSON.stringify(cartCopy)
+    localStorage.setItem("cart", cartString)
+}
+
+useEffect(() => {
+    localCart = JSON.parse(localCart);
+    if (localCart) setCartProducts(localCart)
+}, [])
+
+
+
+ 
 
 console.log('cartProducts ID------', cartProducts.id)
 
@@ -31,25 +71,50 @@ console.log('CartProducts!!!!!!!', cartProducts)
 
 
 return (
-    <div>
+    <div>{cartProducts.length > 0?
+        <>
         <h1>Here's your shopping cart!</h1>
+
         <p></p>
         {cartProducts?.map((shoe) => {
-            return (
+            return (<>
                 <div class="two column row">
                     <div className='ui card'>
                     <div key={shoe.id} >
                 <p><img alt = 'cart shoe' src={shoe.image} width="200" height="200"/></p>
-                <p>{shoe.shoename}</p>
-                <p>{shoe.description}</p>
-                <p>{shoe.price}</p>
+                <h3>{shoe.shoename}</h3>
+                <p>Description: {shoe.description}</p>
+                <p><b>Price: </b> {shoe.price}</p>
+                <p><b>Quantity: </b> {shoe.quantity}</p>
+                <button onClick={(event) => {
+                    event.preventDefault();
+                    console.log('shoeiddddd', shoe.id)
+                    removeItem(shoe.id)
+                }}>Remove from cart</button>
+                <p></p>
                 </div>
                 </div>
+                <div>Total: $10203.00      <button>CheckOut</button></div>
                 </div>
+
+        </>
         )
         
         })}
-        {cartProducts ? <div><button>CheckOut</button>{cartProducts.price}</div> : <h1>Nothing in Cart!</h1>}
+        </>
+        : 
+        <>
+
+        <div>
+        <h1>Cart is empty</h1>
+        <h3>Check out some of our <Link className="item active" to="/shoes">shoes</Link>!</h3>
+        </div>
+
+        
+        
+        </> 
+        }
+        
     </div>
 )
 
